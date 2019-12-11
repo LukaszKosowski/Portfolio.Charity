fbe1e057158505aea2a372c2bee552193a93b00d
Lukasz <kosowski.lukasz.01@gmail.com>
2019-12-11 21:42:59 +0100

    Donation Controller

--- Charity.Mvc/Controllers/DonationController.cs
+++ Charity.Mvc/Controllers/DonationController.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Charity.Mvc.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Charity.Mvc.Controllers
 {
     public class DonationController : Controller
     {
+        private readonly IInstitutionSerwice _instytutionService;
+        private readonly ICategoryService _categoryService;
+
+        public DonationController(IInstitutionSerwice instytutionService, ICategoryService categoryService)
+        {
+            _instytutionService = instytutionService;
+            _categoryService = categoryService;
+        }
         [HttpGet]
         public IActionResult Donate()
         {
