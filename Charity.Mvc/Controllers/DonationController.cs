f78b54d944ed7daaabd6228a587dfb3ee5f3fa3d
Lukasz <kosowski.lukasz.01@gmail.com>
2019-12-11 21:36:11 +0100

    Donation Controller

---
+++ Charity.Mvc/Controllers/DonationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Charity.Mvc.Controllers
+{
+    public class DonationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Donate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Donate(int i)
+        {
+            return View();
+        }
+    }
+}
